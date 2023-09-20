7c3a184073ecb861974e99da49bea663c5e19cde
Miguel Leal <miguel11065@gmail.com>
2023-09-19 23:11:23 -0300

    finalizando o projeto

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,13 @@
+import BotaoAdicionar from '../../components/BotaoAdcionar'
+import BarraLateral from '../../containers/BarraLateral'
+import ListaDeTarefas from '../../containers/ListaDeTarefas'
+
+const Home = () => (
+  <>
+    <BarraLateral mostrarFiltros />
+    <ListaDeTarefas />
+    <BotaoAdicionar />
+  </>
+)
+
+export default Home
